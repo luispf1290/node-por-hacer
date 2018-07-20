5e7b46e84b702b93977b1e4a979436fcd5c4ed97
luis piña <luispia1290@gmail.com>
2018-07-19 21:56:15 -0500

    Primer comit del segundo ejercisio

---
+++ README.md
@@ -0,0 +1,11 @@
+## Aplicacion de comandos en node
+
+este es el segund ejercisio del curso
+
+recuerden instalar los paquetes  de node
+
+
+```
+npm install
+
+```
---
+++ config/yargs.js
@@ -0,0 +1,34 @@
+
+
+const argv = require('yargs')
+
+const descripcion = {
+	demand:true,
+	alias: 'd',
+	desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+  	alias: 'c',
+	default:true,
+	desc: 'Marca como completado o pendiente la tarea'
+};
+
+.command('crear','Crea un elemento por hacer', {
+	descripcion
+})
+.command('actualizar', 'actualiza el estado completado de una tarea',{
+
+	descripcion,
+	completado
+})
+.command('borrar', 'Borra una tarea', {
+	descripcion
+})
+.help()
+.argv;
+
+
+module.exports = {
+	argv
+}
